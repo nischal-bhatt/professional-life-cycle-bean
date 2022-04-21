8ada70d1b55b0f596c5fcb2a8dc8fb55e683e163
nischal <nischal.bhatt.87@gmail.com>
2022-04-21 09:19:49 +0800

    spring bean life cycle mine

---
+++ src/main/java/professional/spring/beans/life/cycle/ProfessionalSpringBeanLifeCycleApplication.java
@@ -0,0 +1,13 @@
+package professional.spring.beans.life.cycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalSpringBeanLifeCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalSpringBeanLifeCycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/professional/spring/beans/life/cycle/ProfessionalSpringBeanLifeCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package professional.spring.beans.life.cycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalSpringBeanLifeCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
